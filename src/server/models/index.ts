8af636d4756e6778bddda7654f5bf277f3838fa6
Phi Pham Nguyen <Phi Pham Nguyen>
2017-04-23 18:10:17 +0700

    Debug In In TypeScript

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -31,12 +31,15 @@ class Database {
       dbConfig.password, dbConfig);
     this._models = ({} as any);
 
-    fs.readdirSync(__dirname).filter((file: string) => {
-      return (file !== this._basename) && (file !== "interfaces");
-    }).forEach((file: string) => {
-      let model = this._sequelize.import(path.join(__dirname, file));
-      this._models[(model as any).name] = model;
-    });
+    // Comment this code when Debug In VSCODE
+    // fs.readdirSync(__dirname).filter((file: string) => {
+    //   return (file !== this._basename) && (file !== "interfaces");
+    // }, (err: Error) => {
+    //   console.log(`Error = ${err} `);
+    // }).forEach((file: string) => {
+    //   let model = this._sequelize.import(path.join(__dirname, file));
+    //   this._models[(model as any).name] = model;
+    // });
 
     Object.keys(this._models).forEach((modelName: string) => {
       if (typeof this._models[modelName].associate === "function") {
@@ -52,6 +55,8 @@ class Database {
   getSequelize() {
     return this._sequelize;
   }
+
+
 }
 
 const database = new Database();
@@ -59,3 +64,20 @@ export const models = database.getModels();
 export const sequelize = database.getSequelize();
 
 
+// Ping checks that the database server can be connected to. It attempts to connect every second for 60 seconds, before giving up and throwing
+// an error back up to the chain.  This function is especially useful when using Docker Compose with a linked DB, which may not always be ready before
+// the API server starts
+// exports async ping = () => {
+//   for (let i = 1; i <= 60; i++) {
+//     try {
+//       await sequelize.authenticate();
+//       return;
+//     } catch (e) {
+//       console.log(`DB connection attempt #${i} failed, retrying...`);
+//       // await delay(1000);
+//     }
+//   }
+
+//   throw new Error("Couldn't connect to database!");
+// }
+// https://github.com/sequelize/sequelize/issues/6524
